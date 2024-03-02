e1ddd23371fffaffe736a51fe9280edc7e6c8342
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2024-03-02 14:34:09 +0000

    done

--- doList.js
+++ doList.js
@@ -13,13 +13,36 @@ button.addEventListener('click',function(event){
     check.id="id"
     let label = document.createElement("label")
    label.htmlFor="id"
-
+    let span = document.createElement("span")
+    span.id='cross';
+    span.innerHTML='\u00d7'  
+    let span1 = document.createElement("img")
+    span1.id='edit';
+    span1.src="edit.svg.png"
+    label.innerHTML= input.value
+    input.value=" "
    
     
     li.appendChild(check);
     li.appendChild(label)
+    li.appendChild(span)
+    li.appendChild(span1)
+  
+
+    span.addEventListener('click',function(e){
+        if(e.target.id==='cross'){
+          let task=this.parentElement;
+           task.remove()
+        }
+    })
+    span1.addEventListener('click',function(e){
+        let taskText = label.innerText; 
+        let newText = prompt("Edit Task", taskText); 
+        if (newText !== null) {
+            label.innerText = newText;
+        }
+    })
     
-   
-   label.innerHTML= input.value
+    
    
 })
